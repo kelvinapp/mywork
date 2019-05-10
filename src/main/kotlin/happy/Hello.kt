8c030772a58960555b34690f38ca8bbefe69aa86
Kelvin Lau <kelvinapp2017@gmail.com>
2019-05-10 17:22:55 +0800

    Initial commit

---
+++ src/main/kotlin/happy/Hello.kt
@@ -0,0 +1,14 @@
+package happy
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+class Human {
+    fun hello(){
+        println("Hello kotlin")
+    }
+
+}
