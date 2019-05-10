ba0b74b2d89ab9a031b632c9fcd327ebd000f5d6
Kelvin Lau <kelvinapp2017@gmail.com>
2019-05-10 22:48:35 +0800

    java and kotlin variable , date type

--- src/main/kotlin/happy/Hello.kt
+++ src/main/kotlin/happy/Hello.kt
@@ -5,6 +5,13 @@ fun main(args: Array<String>) {
 //    Human().hello()
     val h = Human()
     h.hello()
+    var age = 25
+    age = age.plus(other:1)
+    var weight = 66.5f
+    var enroll : Boolcam = true
+    println(age)
+
+
 }
 class Human {
     fun hello(){
